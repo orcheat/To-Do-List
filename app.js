493ff0e397f19fe1653fb2fc8198b52587872e07
Archit <82609753+orcheat@users.noreply.github.com>
2022-08-23 15:28:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+var items=[];
+
+app.get('/', function (req, res) {
+    var today = new Date();
+
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    var day=today.toLocaleDateString("en-US",options);
+    res.render("list", { kindOfDay: day ,newListItem: items});
+});
+
+app.post('/', function (req, res)
+{
+   var item=req.body.newItem;
+   items.push(item);
+    res.redirect("/");
+});
+app.listen(3000, function () {
+    console.log("server started");
+})
